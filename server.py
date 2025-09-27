96f615075e967348037809b1b3cce7da04e54dda
Levithein <sisonam10@gmail.com>
2025-09-27 11:53:11 +0800

    created db file

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from supabase_db import db
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/test")
+def read_test():
+    data = db.table("main").select("*").execute()
+    return {"data": data.data}
