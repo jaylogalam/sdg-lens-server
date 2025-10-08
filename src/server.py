446dc694d528fba3dc843d7bebc256dee8faf8e5
Jaylo <jaylogalam04@gmail.com>
2025-10-08 20:15:34 +0800

    remove test file

--- src/server.py
+++ src/server.py
@@ -7,10 +7,6 @@ app = FastAPI()
 from routers.text import router as text_route
 app.include_router(text_route)
 
-# Tests
-from tests.route import router as test_route
-app.include_router(test_route)
-
 # Root function
 @app.get("/")
 def read_root():
