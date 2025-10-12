9315a76e08bd7ed2dd059090a2ae8a21d9c10741
Jaylo <jaylogalam04@gmail.com>
2025-10-12 13:55:45 +0800

    renamed to limiter

--- src/core/rate_limiting.py
+++
@@ -1,9 +0,0 @@
-# Initialize a rate limiter using SlowAPI.
-
-# `Limiter` manages request rate limits to prevent abuse or overuse of API endpoints.
-from slowapi import Limiter
-
-# `key_func=get_remote_address` means each client's rate limit is tracked based on their IP address.
-from slowapi.util import get_remote_address
-
-limiter = Limiter(key_func=get_remote_address)
