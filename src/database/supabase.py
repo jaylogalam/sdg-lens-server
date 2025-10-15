8da6a35846990c3592cdf0f1e47133ecf0d54ed7
Jaylo <jaylogalam04@gmail.com>
2025-10-15 16:25:05 +0800

    moved to data folder

--- src/database/supabase.py
+++
@@ -1,5 +0,0 @@
-from supabase import create_client, Client
-
-SUPABASE_URL = "https://duxqgefxnyrnlymsauso.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1eHFnZWZ4bnlybmx5bXNhdXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NDAxMzAsImV4cCI6MjA3NDUxNjEzMH0.KpKohNP5fOlcGeC2s2rdArCmsTcZHb9CaX7ez-3Eh0A"
-client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
