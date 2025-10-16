b7bf21334f70e214087017f08ab04d91fde42afd
Jaylo <jaylogalam04@gmail.com>
2025-10-16 21:11:03 +0800

    fix model

--- src/models/auth_models.py
+++ src/models/auth_models.py
@@ -1,13 +1,6 @@
 from pydantic import BaseModel, EmailStr
-from datetime import datetime
 
 class AuthModel:
-    class InitProfile(BaseModel):
-        id: str
-        created_at: datetime
-        username: str
-        email: EmailStr
-        
     class Signup(BaseModel):
         username: str
         email: EmailStr
