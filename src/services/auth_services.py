5135044629dc92d41cff9cc5ffb5bc682c40df06
Jaylo <jaylogalam04@gmail.com>
2025-10-16 19:23:20 +0800

    add initialize profile

--- src/services/auth_services.py
+++ src/services/auth_services.py
@@ -1,16 +1,26 @@
-from typing import Annotated
-from fastapi.params import Depends
-from database import get_db, Client
+from supabase import Client
 from models import AuthModel
 
 class AuthServices:
     class Signup:
         @staticmethod
-        def with_password(creds: AuthModel.Signup, db: Annotated[Client, Depends(get_db)]):
-            db.auth.sign_up({
+        def with_password(creds: AuthModel.Signup, db: Client):
+            response = db.auth.sign_up({
                 "email": creds.email,
                 "password": creds.password
             })
+
+            user = response.user
+            if not user:
+                raise Exception("Signup failed: No user returned")
+
+            init_profile = AuthModel.InitProfile(
+                id=user.id,
+                created_at=user.created_at,
+                username=creds.username
+            )
+
+            response = db.table("profiles").insert(init_profile.model_dump(mode="json")).execute()
 
     class Login:
         ...
