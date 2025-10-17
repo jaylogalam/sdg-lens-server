f2ff72b18ab1f652264d2593d67ac7dd0a9e93ae
Jaylo <jaylogalam04@gmail.com>
2025-10-17 16:59:52 +0800

    fix type errors and 'list' bug

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -1,5 +1,6 @@
-from fastapi import Request, Depends, HTTPException, status
+from fastapi import Request, Response, Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Callable, Awaitable
 import jwt
 
 SUPABASE_JWT = "55S6HwNqDTgu0Gqj0tJqEb4fRepB8l7FcHP+M1UBe+SbBBRKCs+vx80IoD/bMljQD+Taz5hFWCUTAriqAUJzEQ=="
@@ -7,17 +8,18 @@ security = HTTPBearer()
 
 class AuthMiddleware:
     @staticmethod
-    async def __call__(request: Request, call_next):
+    async def __call__(request: Request, call_next: Callable[[Request], Awaitable[Response]]):
         token = request.cookies.get('access_token')
         if token and token.startswith('Bearer '):
             token = token[7:]  # Remove 'Bearer ' prefix
-            request.headers.__dict__['list'].append(
+            request.headers.__dict__['_list'].append(
                 (b"authorization", f"Bearer {token}".encode())
             )
 
         response = await call_next(request)
         return response
-    
+
+    @staticmethod
     def get_user(creds: HTTPAuthorizationCredentials = Depends(security)):
         try:
             token = creds.credentials
