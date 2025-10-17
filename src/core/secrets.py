981027e3d0f6954a944cf8e6bd4d803814368457
Jaylo <jaylogalam04@gmail.com>
2025-10-17 19:55:04 +0800

    add client url

--- src/core/secrets.py
+++ src/core/secrets.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 class Secrets:
+    CLIENT_URL = os.getenv('CLIENT_URL')
     SUPABASE_URL = os.getenv('SUPABASE_URL')
     SUPABASE_KEY = os.getenv('SUPABASE_KEY')
     SUPABASE_JWT = os.getenv('SUPABASE_JWT')
