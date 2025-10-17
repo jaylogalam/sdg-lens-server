f5e0ea2be8e42ee844cc6643a0ba1f661ab88a75
Jaylo <jaylogalam04@gmail.com>
2025-10-17 20:33:29 +0800

    fix circular dependency in dependencies.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,8 @@
 from .secrets import Secrets
 from .limiter import limiter
+
 from .middleware import Middleware, AuthMiddleware
-from .routers import Routers
-from .dependencies import Dependencies
 from .database import Database
+
+from .dependencies import Dependencies
+from .routers import Routers
