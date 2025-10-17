a90994c8449dde45ce2681c72a61ae534336b4ae
Jaylo <jaylogalam04@gmail.com>
2025-10-17 21:10:10 +0800

    fix profile route retrieving user data

--- src/services/profile_services.py
+++ src/services/profile_services.py
@@ -1,17 +1,12 @@
 from supabase import Client
-from models import ProfileModel
 
 class ProfileServices:
     @staticmethod
-    def get_profile(db: Client,  user_id: str):
-        session = db.auth.get_user()
-        return session
+    def get_profile_data(db: Client, user_id: str):
+        response = db.table("profiles").select("*").eq("id", user_id).limit(1).execute()
+        results = getattr(response, "data", None)
+
+        if not results or len(results) == 0:
+            raise ValueError("No data")
         
-        results = db.table("profiles").select("*").eq("id", user_id).limit(1).execute()
-        data = getattr(results, "data", None)
-        
-        if not data or len(data) == 0:
-            raise ValueError("Profile not found")
-        
-        profile_data = data[0]
-        # return ProfileModel(**profile_data)
+        return results
