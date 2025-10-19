b716270a9d471b11a549acccbd76b899b16d4bb2
Jaylo <jaylogalam04@gmail.com>
2025-10-19 13:55:13 +0800

    add analyze router

---
+++ src/routers/analyze_router.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Request
+from core import limiter, Dependencies
+from services import AnalyzeServices
+from models import AnalyzeModel
+
+router = APIRouter(
+    prefix="/analyze"
+)
+    
+@router.get("")
+@limiter.limit("1/second") # type: ignore
+def analyze_text(
+    request: Request,
+    text: AnalyzeModel.Text,
+    classifier: Dependencies.GetClassifier
+):
+    try:
+        return AnalyzeServices.analyze_text(
+            text_input=text.text,
+            classifier=classifier
+        )
+        
+    except Exception as e:
+        return {"error": str(e)}
