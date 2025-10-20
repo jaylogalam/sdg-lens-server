53f34f821d996434f4289ac8ccbcb1d258b36d49
Jaylo <jaylogalam04@gmail.com>
2025-10-20 14:52:28 +0800

    new pipeline connecting to hugging face inference api

--- src/core/secrets.py
+++ src/core/secrets.py
@@ -4,6 +4,8 @@ import os
 load_dotenv()
 
 CLIENT_URL = os.getenv('CLIENT_URL')
+PIPELINE_URL = os.getenv('PIPELINE_URL')
+PIPELINE_KEY = os.getenv('PIPELINE_KEY')
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 SUPABASE_JWT = os.getenv('SUPABASE_JWT')
