e2769297a52eb9fd9d9208ec35dfb840b2c014a4
Jaylo <jaylogalam04@gmail.com>
2025-10-20 14:52:08 +0800

    remove server model

--- src/core/events.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from core.pipeline import Pipeline
-
-class Startup:
-    @staticmethod        
-    def register(app: FastAPI):
-        app.add_event_handler("startup", Pipeline.load_classifier_model) # type: ignore
--- src/server.py
+++ src/server.py
@@ -1,12 +1,9 @@
 # This is the main entry point of the FastAPI server.
 from fastapi import FastAPI
-from core.events import Startup
 from core.middleware import Middleware
 from core.routers import Routers
 
 app = FastAPI()
-
-Startup.register(app)
 
 # Register middlewares
 Middleware.register(app)
