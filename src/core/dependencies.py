104d1ce24bda613c160b64fad775b6118e7a146a
Jaylo <jaylogalam04@gmail.com>
2025-10-26 14:25:45 +0800

    remove getuser dependency

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,10 +1,8 @@
 from fastapi import Depends
-from typing import Annotated, Any
+from typing import Annotated
 from supabase import Client
 
-from core.middleware import AuthMiddleware
 from core.database import Database
 
-GetUser = Annotated[Any, Depends(AuthMiddleware.get_user)]
 GetDB = Annotated[Client, Depends(Database.get_db)]
 GetDBAdmin = Annotated[Client, Depends(Database.get_db_admin)]
