9e8b50d2c833e177d113de9f8251a754ef5fcab8
Jaylo <jaylogalam04@gmail.com>
2025-11-16 13:48:53 +0800

    fixed db dependencies

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from core.dependencies import GetDB, GetDBAdmin
+from db.dependencies import GetDB
 from core.limiter import limiter
 from services.auth_services import AuthServices
 from models import AuthModel
@@ -10,11 +10,10 @@ router = APIRouter(
     
 @router.post("/signup")
 @limiter.limit("1/second") # type: ignore
-def signup(request: Request, creds: AuthModel.Signup, db: GetDB, admin: GetDBAdmin):
+def signup(request: Request, creds: AuthModel.Signup, db: GetDB):
     try:
         return AuthServices.Signup.with_password(
             db=db,
-            admin=admin,
             username=creds.username,
             email=creds.email,
             password=creds.password
@@ -25,11 +24,10 @@ def signup(request: Request, creds: AuthModel.Signup, db: GetDB, admin: GetDBAdm
 
 @router.post("/login")
 @limiter.limit("1/second") # type: ignore
-def login(request: Request, creds: AuthModel.Login, db: GetDB, admin: GetDBAdmin):
+def login(request: Request, creds: AuthModel.Login, db: GetDB):
     try:
         auth_response = AuthServices.Login.with_password(
             db=db,
-            admin=admin,
             email=creds.email,
             password=creds.password
         )
