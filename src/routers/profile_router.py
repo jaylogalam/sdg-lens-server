eac9ef4666944ed9b6f6b7a461b1de4bc026ba30
Jaylo <jaylogalam04@gmail.com>
2025-11-16 13:50:50 +0800

    remove edit username route

--- src/routers/profile_router.py
+++ src/routers/profile_router.py
@@ -1,31 +1,18 @@
 from fastapi import APIRouter, Request
-from core.dependencies import GetDB, GetDBAdmin
+from db.dependencies import GetDB
 from services.profile_services import ProfileServices
-from models.profile_models import ProfileModel
+from core.limiter import limiter
 
 router = APIRouter(
     prefix="/profile"
 )
 
 @router.get("/")
+@limiter.limit("1/second") # type: ignore
 def get_profile(request: Request, db: GetDB):
     try:
         response = ProfileServices.get_profile_data(db)
         return response
         
-    except Exception as e:
-        return {"error": str(e)}
-
-@router.post("/edit_username")
-def edit_username(request: Request, profile: ProfileModel.Edit, db: GetDB, admin: GetDBAdmin):
-    try:
-        response = ProfileServices.edit_username(
-            db = db,
-            admin = db,
-            name = profile.username
-        )
-        
-        return response
-
     except Exception as e:
         return {"error": str(e)}
