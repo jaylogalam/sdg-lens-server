0470f9f11c2e95fc6df5de6e9e74e0592504535e
Jaylo <jaylogalam04@gmail.com>
2025-11-17 22:34:11 +0800

    fix metadata insertion for create_user modified read_user and read_users

--- src/services/admin_services.py
+++ src/services/admin_services.py
@@ -1,23 +1,36 @@
 from supabase import Client
+from utils.admin import AdminUtils
 
 class AdminServices:
     @staticmethod
-    def create_user(db: Client, email: str, password: str, user_metadata: dict[str, str]):
+    def create_user(db: Client, email: str, password: str, username: str):
         return db.auth.admin.create_user(
         {
             "email": email,
             "password": password,
-            "user_metadata": user_metadata,
+            "user_metadata": {
+                "username": username,
+                "app_role": "user",
+            },
         }
     )
         
     @staticmethod
-    def read_user(db: Client, id: str | None = None):
-        if not id:
-            return db.auth.admin.list_users()
+    def read_user(db: Client, id: str):
+        user = db.auth.admin.get_user_by_id(id).user
+        user_data = AdminUtils.format_user_data(user) # type: ignore
 
-        return db.auth.admin.get_user_by_id(id)
+        return user_data
 
+    @staticmethod
+    def read_users(db: Client):
+        users = db.auth.admin.list_users()
+        response: list[dict[str, str]] = []
+        for user in users:
+            response.append(AdminUtils.format_user_data(user)) # type: ignore
+            
+        return response
+    
     @staticmethod
     def update_user(db: Client, id: str, data: dict[str, str]):
         return db.auth.admin.update_user_by_id(id, data) # type: ignore
