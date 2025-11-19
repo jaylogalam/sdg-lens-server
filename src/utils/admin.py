3c97faad838c8484b7476f1d27c4cc3fce8fc214
Jaylo <jaylogalam04@gmail.com>
2025-11-19 14:19:59 +0800

    add fallback on missing data

--- src/utils/admin.py
+++ src/utils/admin.py
@@ -16,12 +16,12 @@ class AdminUtils:
         DATE_FORMAT = "%c"
         user_data: dict[str, str] = {
             "user_id": user.id,
-            "email": user.email,
-            "username": user.user_metadata.get('username'),
-            "app_role": user.user_metadata.get('app_role'),
-            "created_at": user.created_at.strftime(DATE_FORMAT),
-            "last_sign_in_at": user.last_sign_in_at.strftime(DATE_FORMAT), # type: ignore
-            "updated_at": user.updated_at.strftime(DATE_FORMAT) # type: ignore
+            "email": user.email if user.email else "",
+            "username": user.user_metadata.get('username') if user.user_metadata.get('username') else "",
+            "app_role": user.user_metadata.get('app_role') if user.user_metadata.get('app_role') else "",
+            "created_at": user.created_at.strftime(DATE_FORMAT) if user.created_at else "",
+            "last_sign_in_at": user.last_sign_in_at.strftime(DATE_FORMAT) if user.last_sign_in_at else "", # type: ignore
+            "updated_at": user.updated_at.strftime(DATE_FORMAT) if user.updated_at else "" # type: ignore
         }
 
         return user_data
