fbee6a15762a52bda171fae34ee3738cfd8f496d
Jaylo <jaylogalam04@gmail.com>
2025-11-20 23:32:24 +0800

    add get id dependency

--- src/db/dependencies.py
+++ src/db/dependencies.py
@@ -1,7 +1,8 @@
 from fastapi import Depends
 from typing import Annotated
 from supabase import Client
-from db.supabase import get_db, get_db_admin
+from db.supabase import get_db, get_db_admin, get_id
 
 GetDB = Annotated[Client, Depends(get_db)]
 GetDBAdmin = Annotated[Client, Depends(get_db_admin)]
+GetUID = Annotated[Client, Depends(get_id)]
