b8d28abb7f8a10c80d2a76f2853247f99eb41889
Jaylo <jaylogalam04@gmail.com>
2025-11-21 12:08:44 +0800

    remove uid

--- src/routers/auth_router.py
+++ src/routers/auth_router.py
@@ -33,7 +33,7 @@ def signup(request: Request, creds: AuthModel.Signup, db: GetDB):
 
 @router.post("/login")
 @limiter.limit("1/second") # type: ignore
-def login(request: Request, creds: AuthModel.Login, db: GetDB, uid: GetUID):
+def login(request: Request, creds: AuthModel.Login, db: GetDB):
     try:
         auth_response = AuthServices.Login.with_password(
             db=db,
