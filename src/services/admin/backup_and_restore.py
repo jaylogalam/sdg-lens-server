668277834548c1cb3e918d7c55580ba8f2206f43
Jaylo <jaylogalam04@gmail.com>
2025-11-21 14:05:33 +0800

    add backup and restore services

---
+++ src/services/admin/backup_and_restore.py
@@ -0,0 +1,31 @@
+from core.secrets import SUPABASE_POSTGRESQL
+from supabase import Client
+import pandas as pd
+from datetime import datetime
+from pathlib import Path
+
+if not SUPABASE_POSTGRESQL:
+    raise ValueError("SUPABASE_POSTGRESQL environment variable not loaded")
+
+tables = [
+    {"schema": "public", "table": "profiles"},
+    {"schema": "public", "table": "history"},
+    {"schema": "public", "table": "logs"},
+]
+
+class Backup:
+    @staticmethod
+    def create(db: Client):
+        for table in tables:
+            response = db.table(table["table"]).select("*").execute()
+            
+            if not response:
+                raise RuntimeError(f"Error getting data from {table["schema"]}.{table['table']}")
+
+            df = pd.DataFrame(response.data)
+
+            date = datetime.now()
+            date = str(date.strftime("%Y;%m;%d-%H;%M;%S"))
+            save_path = Path(f'{date}-{table["schema"].upper()}{table['table']}.csv')
+            save_path.parent.mkdir(parents=True, exist_ok=True)
+            df.to_csv(save_path, index=False)
